9c215ee9fbed6907795f7a6146d9dde677caa326
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2019-03-24 08:01:15 +0000

    include link as an argument

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import praw
+import sys
 from praw.models import MoreComments
 
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)', client_id='mslOZtKlprikhQ', client_secret='Hhe0vuQDqF7lWOYGLmRe2nKejJg')
 
-submission = reddit.submission(url='https://www.reddit.com/r/cscareerquestions/comments/b469n3/why_are_recruiters_insistent_on_having_phone/')
-print(submission.title)
-print(submission.selftext)
-for top_level_comment in submission.comments:
-    if isinstance(top_level_comment, MoreComments):
-        continue
-    print(top_level_comment.body)
+
+arguments = sys.argv[1:]
+for url in arguments:
+    submission = reddit.submission(url)
+    print(submission.title)
+    print(submission.selftext)
+    for top_level_comment in submission.comments:
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        print(top_level_comment.body)
